5dbabefcfdbc80fdb80202c88e0629ad7372afaf
tsun165 <sondo16052004@gmail.com>
2025-12-23 14:04:36 +0700

    refactor(Map); split StationDetail

--- frontend/src/components/map/index.js
+++ frontend/src/components/map/index.js
@@ -4,6 +4,5 @@ export { default as MapControls } from './MapControls';
 export { default as MapWebView } from './MapWebView';
 export { default as SearchBar } from './SearchBar';
 export { default as SearchResults } from './SearchResults';
-export { default as StationBottomSheet } from './StationBottomSheet';
-export { default as StationDetail } from './StationDetail';
+export { default as StationDetailSheet } from './StationDetailSheet';
 
